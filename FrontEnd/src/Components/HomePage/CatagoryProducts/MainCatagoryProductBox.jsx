9351a7e72464024730a7893ac3e10fe6d33d4871
Abir Barai <abirbarai08@gmail.com>
2025-09-14 23:07:20 +0530

    Change the useEffect to productStore

--- FrontEnd/src/Components/HomePage/CatagoryProducts/MainCatagoryProductBox.jsx
+++ FrontEnd/src/Components/HomePage/CatagoryProducts/MainCatagoryProductBox.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from 'react';
 import CatagoryProducts from './CatagoryProducts.jsx';
 import { v4 as uuidv4} from 'uuid';
+import useProductStore from '../../../Store/productStore.js';
 
 export default function MainCatagoryProductBox() {
-    const [products, setProducts] = useState([]);
+    const fetchAllProducts = useProductStore((state) => state.fetchAllProducts);
+    const products = useProductStore((state) => state.products);
     const [loading , setLoading] = useState(true);
 
     const catagories = ["Fashion" , "Mobiles", "Electronics", "Home & Furniters" , "Appliances", "Toys"]
 
     useEffect(() => {
-        fetch("https://e-commerce-website-1-g5ui.onrender.com")
-            .then(res => res.json())
-            .then(data => {
-                setProducts(data.products)
-                setLoading(false)});
-    }, []);
+        const allProducts = async () => {
+            await fetchAllProducts();
+            setLoading(false)
+        };
+        allProducts();
+    }, [fetchAllProducts]);
 
     return (
         <>
