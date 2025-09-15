662bbe7317b30c574cd1581b9e9b2a9fdefccd12
Abir Barai <abirbarai08@gmail.com>
2025-09-15 20:00:38 +0530

    delete expires for cookie

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -44,7 +44,6 @@ const sessionOptions = {
     ttl: 7 * 24 * 60 * 60
   }),
   cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: true,
