ea0eb53c48454a62f0a550034d5eda70045bbdd7
AbirBarai08 <abirbarai08@gmail.com>
2025-10-27 10:56:28 +0530

    Implement error handling in sendEmail function
    Added error handling for email sending process.

--- BackEnd/Utils/sendEmail.js
+++ BackEnd/Utils/sendEmail.js
@@ -16,7 +16,16 @@ const sendEmail = async (to, message) => {
         text: message,
     };
 
-    await transporter.sendMail(mailOptions);
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log("Mail sent");
+    } catch (err) {
+      console.error("Mail error:", err.message);
+      return res.status(500).json({ message: "Failed to send OTP", error: err.message });
+    }
+
+res.status(200).json({ message: "OTP sent successfully" });
+
 };
 
 module.exports = sendEmail;
